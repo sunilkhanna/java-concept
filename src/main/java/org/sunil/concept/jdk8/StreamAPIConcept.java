54b6d6462bb16bfd4f4f1166eead107112ebf4e5
sunil khanna <sunil.khanna.c@ephesoft.com>
2022-06-24 13:34:35 +0530

    Java 8 Stream API concept

--- src/main/java/org/sunil/concept/jdk8/StreamAPIConcept.java
+++ src/main/java/org/sunil/concept/jdk8/StreamAPIConcept.java
@@ -26,6 +26,73 @@ public class StreamAPIConcept {
     list.add(e1);
     list.add(e2);
 
+    
+    //------print a employee name that begins with 'S' -----//
+    
+    list.stream().filter(emp -> emp.getName().startsWith("s")).
+    forEach(e -> {System.out.println(e.getName());});
+    
+    
+   // list.stream().mapToDouble(e->e.getAge()).forEach(e->{System.out.println(e);});
+    
+    //----------Sort employee by AGE-------//
+    
+    list.stream().sorted(new Comparator<Employee>() {public int compare(Employee o1, Employee o2) {
+      return 0;};});
+    
+    list.stream().sorted((o1, o2) -> (o1.getAge() - o2.getAge())).collect(Collectors.toList())
+        .forEach(e -> {
+          System.out.println(e.getName() + "-" + e.getAge());
+        });
+    
+/**    
+    //1st way using comparator....(by this  way processing list itself sorted)
+    
+    list.sort(new Comparator<Employee>() {
+
+      @Override
+      public int compare(Employee o1, Employee o2) {
+        
+        Integer x1=o1.getAge();
+        Integer x2=o2.getAge();
+        
+        return x1.compareTo(x2);
+      }});
+    
+    for(Employee e:list) {
+      
+      System.out.println(e.getName()+" "+e.getAge());
+      
+    }
+    
+    persons.sort((p1, p2) -> {
+        if (p1.getName().compareTo(p2.getName()) == 0) {
+            return p1.getAge().compareTo(p2.getAge());
+        } else {
+            return p1.getName().compareTo(p2.getName());
+        } 
+    });
+    
+*/    
+    
+  //--------stream appraoch---------//
+    System.out.println("*****List sorting using Method reference and STREAM****");
+  list.stream().sorted(Comparator.comparing(Employee::getName).thenComparing(Employee::getAge))
+      .forEach(emp->{System.out.println(emp.getName()+"--"+emp.getAge());});
+    
+    
+    
+    
+    
+    
+    
+  System.out.println("*********");
+    
+    
+  /*
+   * sunil--10 asnill--30 juhnti--40 daswse--50 sajnsesew--60
+   */
+    
     // -----------get a list of ages---------------------//
     //----------map  and flatten map
 
