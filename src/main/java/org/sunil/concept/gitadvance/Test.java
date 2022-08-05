86f15bdbecf11898c92de7f81d92823d01205ad3
sunilpersonal <er.sunilkhanna@gmail.com>
2022-08-05 18:05:33 +0530

    second commit-git advance

--- src/main/java/org/sunil/concept/gitadvance/Test.java
+++ src/main/java/org/sunil/concept/gitadvance/Test.java
@@ -3,8 +3,9 @@ package org.sunil.concept.gitadvance;
 public class Test {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
 
+	System.out.println("second comit");
+	
     }
 
 }
